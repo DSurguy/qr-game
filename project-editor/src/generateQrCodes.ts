6af3fdaa01894e803a533b417cd3d7c695ddc67b
Derek Surguy <Surguy.Derek@gmail.com>
2022-07-04 10:37:04 -0500

    Generate qr codes in grid for all players

--- project-editor/src/generateQrCodes.ts
+++ project-editor/src/generateQrCodes.ts
@@ -1,27 +1,54 @@
-import { createWriteStream } from 'node:fs';
 import { resolve } from 'node:path';
-import { toCanvas } from 'qrcode';
-import { createCanvas } from 'canvas';
-import pdfjs from 'pdfjs';
+import { toDataURL } from 'qrcode';
+import JSPDF from 'jspdf';
+
+const letterSizeRatio = 279/216; //in mm
+const ptScale = 1000 / 279
+const millisPerPoint = 0.352778 * ptScale;
 
 export async function generateQrCodes(project: ProjectDefinition) {
-  let imageBuffer: Buffer;
   try {
-    const canvas = createCanvas(400, 400)
-    const codeCanvas: any = await toCanvas(canvas, project.players[0].uuid)
-    imageBuffer = codeCanvas.toBuffer("image/jpeg");
-  } catch (e) {
-    console.log("Error creating qr code canvas");
-    throw e;
-  }
-  try {
-    const doc = new pdfjs.Document()
-    doc.pipe(createWriteStream(resolve(__dirname, 'output/qr.pdf')))
+    const pageFormat = [1000, letterSizeRatio*1000];
+    const doc = new JSPDF({
+      format: pageFormat
+    })
+    doc.setFontSize(40)
+    
+    const numBlocks = 4;
+    const blocksPerPage = numBlocks*numBlocks;
+    const blockWidth = 250;
+    const blockHeight = 300;
+    const qrSize = 200;
 
-    const pdfImg = new pdfjs.Image(imageBuffer);
-    doc.image(pdfImg);
+    for( let i=0; i<project.players.length; i++ ){
+      if( i%(numBlocks*numBlocks) === 0 && i > 0 ) doc.addPage(pageFormat)
+      const pageContextIndex = i % blocksPerPage;
 
-    await doc.end()
+      let qrCodeDataUrl;
+      try {
+        qrCodeDataUrl = await toDataURL(project.players[i].uuid)
+      } catch (e) {
+        console.log("Error creating qr code data url");
+        throw e;
+      }
+
+      const blockXPos = Math.floor(pageContextIndex%numBlocks) * blockWidth;
+      const blockYPos = Math.floor(pageContextIndex/numBlocks) * blockHeight;
+
+      doc.addImage(
+        qrCodeDataUrl,
+        'JPEG',
+        blockXPos + (blockWidth-qrSize)/2,
+        blockYPos,
+        qrSize,
+        qrSize
+      )
+
+      doc.text(project.players[i].wordId, blockXPos + blockWidth/2, blockYPos + qrSize + 10, {
+        align: 'center'
+      })
+    }
+    doc.save(resolve(__dirname, 'output/qr.pdf'))
   } catch (e) {
     console.log("Error creating and writing to pdf");
     throw e;
