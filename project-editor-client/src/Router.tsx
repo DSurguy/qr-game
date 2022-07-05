3b04d694fb474af8b1d2b7b0847b6a1ab1d5a9a9
Derek Surguy <Surguy.Derek@gmail.com>
2022-07-05 14:39:31 -0500

    project-editor-client: add mantine UI and remove tailwindcss reset

--- project-editor-client/src/Router.tsx
+++ project-editor-client/src/Router.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HomeRoute } from './routes/Home';
 
 const MockRoute = () => <p className="font-bold text-teal-500">hi mom</p>
 
 export function Router(){
   return <BrowserRouter>
     <Routes>
-      <Route path="/" element={<MockRoute />} />
+      <Route path="/" element={<HomeRoute />} />
     </Routes>
   </BrowserRouter>
 }
