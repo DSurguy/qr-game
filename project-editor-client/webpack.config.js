3305b85b26c21495e325064bf9d17a8683c9b24e
Derek Surguy <Surguy.Derek@gmail.com>
2022-07-05 14:16:59 -0500

    Add fastify to project-editor, create project-editor-client and use it as static files

---
+++ project-editor-client/webpack.config.js
@@ -0,0 +1,48 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.tsx',
+  output: {
+    path: path.resolve(__dirname, 'dist')
+  },
+  resolve: 
+  {
+    extensions: [ '.tsx', '.ts', '.js', '.json', '.jsx' ],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        include: path.resolve(__dirname, 'src'),
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+      {
+        test: /\.(ts|tsx)$/,
+        include: path.resolve(__dirname, 'src'),
+        exclude: /node_modules/,
+        use: ["ts-loader"],
+      },
+      {
+        test: /\.css$/i,
+        include: path.resolve(__dirname, 'src'),
+        use: ['style-loader', 'css-loader', 'postcss-loader']
+      },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+        type: 'asset',
+      },
+    ]
+  },
+  devServer: {
+    static: './dist',
+    client: {
+      overlay: false
+    }
+  },
+  devtool: 'eval-source-map',
+  plugins: [
+    new HtmlWebpackPlugin()
+  ]
+}
