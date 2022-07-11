e6f7dd6e0cc34886732df0b7042bc767b8fe27a2
Derek Surguy <Surguy.Derek@gmail.com>
2022-07-11 09:37:55 -0500

    Implement project endpoints

--- server/src/routing/adminRouter.ts
+++ server/src/routing/adminRouter.ts
@@ -17,7 +17,8 @@ export const adminRouter: FastifyPluginCallback = (app, options, done) => {
       reply.code(500).send();
     }
   })
-  app.post<{ Body: UnsavedProjectType, Reply: SavedProjectType }>('/project', (req, reply) => {
+
+  app.post<{ Body: UnsavedProjectType, Reply: SavedProjectType }>('/projects', (req, reply) => {
     const {
       name,
       description
@@ -25,7 +26,7 @@ export const adminRouter: FastifyPluginCallback = (app, options, done) => {
     const uuid = randomUUID();
     const getRandomListItem = (list: string[]) => list[getRandomInt(0, list.length)];
     const wordId = getRandomListItem(adjectives) + getRandomListItem(adjectives) + getRandomListItem(animals);
-    const insert = app.db.prepare(`INSERT INTO projects (uuid, wordId, name, description) VALUES (?,?,?,?)`)
+    const insert = app.db.prepare(`INSERT INTO projects (uuid, wordId, name, description, deleted) VALUES (?,?,?,?, 0)`)
     try {
       insert.run(uuid, wordId, name, description)
       reply.status(201).send({
@@ -39,5 +40,75 @@ export const adminRouter: FastifyPluginCallback = (app, options, done) => {
       reply.status(500).send()
     }
   })
+
+  //TODO: Paginate
+  app.get<{ Reply: SavedProjectType[], Querystring: { deleted?: boolean } }>('/projects', (req, reply) => {
+    try {
+      const retrieve = app.db.prepare(`SELECT uuid, wordId, name, description FROM projects WHERE deleted = ?`);
+      const results = retrieve.all(req.query.deleted !== undefined ? 1 : 0);
+      reply.status(200).send(results);
+    } catch (e) {
+      console.error(e);
+      reply.status(500).send();
+    }
+  })
+
+  app.put<{ Body: SavedProjectType, Reply: SavedProjectType, Params: { projectUuid: string } }>('/projects/:projectUuid', {
+    preHandler: (req, reply, done) => {
+      if( req.body.uuid !== req.params.projectUuid ) done(new Error("uuid in payload does not match URL"))
+      else done();
+    }
+  }, (req, reply) => {
+    try {
+      const {
+        uuid,
+        wordId,
+        name,
+        description
+      } = req.body;
+      const update = app.db.prepare(`UPDATE projects SET name=?, description=? WHERE uuid = ? AND deleted = 0`)
+      const result = update.run(name, description, uuid)
+      if( result.changes === 0 ){
+        //no change made, report this
+        reply.status(404).send()
+      }
+      else {
+        reply.status(200).send({
+          uuid,
+          wordId,
+          name,
+          description
+        })
+      }
+    } catch (e) {
+      console.error(e);
+      reply.status(500).send()
+    }
+  })
+
+  app.delete<{ Params: { projectUuid: string } }>('/projects/:projectUuid', (req, reply) => {
+    try {
+      const del = app.db.prepare(`UPDATE projects SET deleted = 1 WHERE uuid = ? AND deleted = 0`)
+      const result = del.run(req.params.projectUuid);
+      if( result.changes === 0 ) reply.status(404).send()
+      else reply.status(200).send()
+    } catch (e) {
+      console.error(e);
+      reply.status(500).send();
+    }
+  })
+
+  app.post<{ Body: { uuid: string }}>('/projects/restore', (req, reply) => {
+    try {
+      const undelete = app.db.prepare(`UPDATE projects SET deleted = 0 WHERE uuid = ? AND deleted = 1`)
+      const result = undelete.run(req.body.uuid)
+      if( result.changes === 0 ) reply.status(404).send()
+      else reply.status(200).send()
+    } catch (e) {
+      console.error(e);
+      reply.status(500).send()
+    }
+  })
+
   done()
 }
