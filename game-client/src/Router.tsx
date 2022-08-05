d8c1fc8da77091c929b637dd43e511c7003072dd
Derek Surguy <Surguy.Derek@gmail.com>
2022-08-04 22:00:44 -0500

    Implement camera test to highlight QR Code

--- game-client/src/Router.tsx
+++ game-client/src/Router.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import CameraTestRoute from './routes/CameraTest';
 
 const MockRoute = () => <p className="font-bold text-teal-500">hi mom</p>
 
@@ -7,6 +8,7 @@ export function Router(){
   return <BrowserRouter>
     <Routes>
       <Route path="/" element={<MockRoute />} />
+      <Route path="/cam" element={<CameraTestRoute />} />
     </Routes>
   </BrowserRouter>
 }
