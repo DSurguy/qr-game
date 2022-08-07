0586d9401c09bc19a89d8d40cff3ca1dcc2a045c
Derek Surguy <Surguy.Derek@gmail.com>
2022-08-07 11:45:43 -0500

    Add favicon to game client

--- game-client/src/index.tsx
+++ game-client/src/index.tsx
@@ -4,8 +4,20 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Router } from './Router';
 import './normalize.css';
+import favicon from './assets/favicon.png';
 
-const appRoot = createRoot(document.querySelector('#app-container'))
-appRoot.render(<MantineProvider>
-  <Router />
-</MantineProvider>)
+//<link rel="icon" href="favicon.ico" type="image/x-icon">
+const linkTag = document.createElement('link');
+linkTag.setAttribute('rel', 'icon')
+linkTag.setAttribute('href', favicon)
+linkTag.setAttribute('type', 'image/png')
+document.querySelector('head')?.appendChild(linkTag)
+
+const appContainer = document.querySelector('#app-container');
+if( appContainer ) {
+  const appRoot = createRoot(appContainer)
+  appRoot.render(<MantineProvider>
+    <Router />
+  </MantineProvider>)
+}
+else console.error("Unable to find #app-container in document")
