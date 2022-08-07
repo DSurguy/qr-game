ddad7d41727e0412e92af97200eef0859220052d
Derek Surguy <Surguy.Derek@gmail.com>
2022-08-07 11:45:05 -0500

    Add favicon to admin client

--- admin-client/src/index.tsx
+++ admin-client/src/index.tsx
@@ -4,6 +4,14 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Router } from './Router';
 import './normalize.css';
+import favicon from './assets/favicon.png';
+
+//<link rel="icon" href="favicon.ico" type="image/x-icon">
+const linkTag = document.createElement('link');
+linkTag.setAttribute('rel', 'icon')
+linkTag.setAttribute('href', favicon)
+linkTag.setAttribute('type', 'image/png')
+document.querySelector('head')?.appendChild(linkTag)
 
 const appRoot = createRoot(document.querySelector('#app-container'))
 appRoot.render(<MantineProvider>
