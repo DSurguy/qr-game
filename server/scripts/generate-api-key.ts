15ab9fc5462966388c252b303944422ec08dcf85
Derek Surguy <Surguy.Derek@gmail.com>
2022-10-09 22:40:55 -0500

    Add api key gen script

---
+++ server/scripts/generate-api-key.ts
@@ -0,0 +1,29 @@
+import { writeFile, rename, rm } from "node:fs/promises";
+import { randomUUID } from "node:crypto";
+import { resolve } from "path";
+
+(async () => {
+  const newKey = randomUUID();
+
+  //back up the existing file first
+  try {
+    await rm(resolve(__dirname, '../conf/nginx-api-key.old'))
+  } catch {}
+  try {
+    await rename(resolve(__dirname, '../conf/nginx-api-key'), resolve(__dirname, '../conf/nginx-api-key.old'))
+  } catch (e) {
+    console.warn("Unable to back up existing nginx conf, it may not exist", e.message);
+  }
+
+  try {
+    await writeFile(resolve(__dirname, '../conf/nginx-api-key'),
+`if ($http_api-key != "${newKey}") {
+  return 401;
+}
+`)
+  } catch (e) {
+    console.error("Unable to create nginx config block");
+  }
+
+  console.log("Done");
+})()
