34f431c4df765c26f286b5bde111e7f8a1de8671
Derek Surguy <Surguy.Derek@gmail.com>
2022-10-16 21:28:32 -0500

    Display a sane title

--- game-client/src/components/layout/AuthHeader.tsx
+++ game-client/src/components/layout/AuthHeader.tsx
@@ -1,12 +1,22 @@
 import React from 'react';
 import { Burger, Button, Header, Text, useMantineTheme } from '@mantine/core';
 import { Camera } from 'tabler-icons-react'
+import { matchPath, useLocation } from 'react-router-dom';
 
 type Props = {
   openCaptureQrModal: () => void;
   openNavbar: () => void;
   closeNavbar: () => void;
   navbarOpen: boolean;
+}
+
+const getTitle = () => {
+  const location = useLocation();
+  if( matchPath({ path: '/game/inventory', end: false}, location.pathname) ) return 'Inventory';
+  if( matchPath({ path: '/game/store', end: false}, location.pathname) ) return 'Store';
+  if( matchPath({ path: '/game/me', end: false}, location.pathname) ) return 'Profile';
+  if( matchPath({ path: '/game/player', end: false}, location.pathname) ) return 'Player';
+  if( matchPath({ path: '/game/duels', end: false}, location.pathname) ) return 'Duels';
 }
 
 export default function AuthHeader({ openCaptureQrModal, openNavbar, closeNavbar, navbarOpen }: Props) {
@@ -21,7 +31,9 @@ export default function AuthHeader({ openCaptureQrModal, openNavbar, closeNavbar
       backgroundColor: theme.colors.dark[8]
     }}>
       <Burger opened={navbarOpen} onClick={() => navbarOpen ? closeNavbar() : openNavbar() } />
-      <Text component="h1" sx={{ fontSize: '1.5rem', margin: 0, marginLeft: '10px', padding: 0 }}>QR Game</Text>
+      <Text component="h1" sx={{ fontSize: '1.5rem', margin: 0, marginLeft: '10px', padding: 0 }}>
+        {getTitle()}
+      </Text>
       <Button sx={{ marginLeft: 'auto' }} onClick={openCaptureQrModal}>
         <Camera size={28} />
       </Button>
